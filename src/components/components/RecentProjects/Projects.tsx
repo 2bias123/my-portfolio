b3966f8aa22534f564ca6b87233e394a401584e6
Tobias <tin017@uib.no>
2023-08-10 23:47:48 +0200

    Added some effects to the project cards

--- src/components/components/RecentProjects/Projects.tsx
+++ src/components/components/RecentProjects/Projects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProjectsStyle.css"
 import tauri from "./tauri.jpg"
 import codeSnippet from "./image-1.png"
+import arrow from "./arrow-up.svg"
 
 
 const Projects: React.FC = () => {
@@ -9,7 +10,7 @@ const Projects: React.FC = () => {
         <section className="projects" id="projects">
             <h2 className="projects-title">Noen av mine nylige prosjekter</h2>
             <div className="projects-container">
-                <div className="project-container project-card">
+                <a href="https://github.com/2bias123/first-tauri" target="_blank" className="project-container" rel="noreferrer">
                     <img 
                     src={tauri}
                     alt="project" 
@@ -18,9 +19,9 @@ const Projects: React.FC = () => {
                     />
                     <h3 className="project-title">Tauri Graftegner</h3>
                     <p className="project-description">Et enkelt graftegnerprogram med backend i Rust og frontend i React.ts.</p>
-                    <a href="https://github.com/2bias123/first-tauri" className="project-link">Vis Prosjektet</a>
-                </div>
-                <div className="project-container project-card">
+                    <a href="https://github.com/2bias123/first-tauri" target="_blank" className="project-link" rel="noreferrer">Ta en titt</a><img src={arrow} alt="arrow" className="arrow" />
+                </a>
+                <a href="https://github.com/2bias123/my-portfolio" target="_blank" className="project-container" rel="noreferrer">                    
                     <img 
                     src={codeSnippet}
                     alt="project" 
@@ -29,8 +30,8 @@ const Projects: React.FC = () => {
                     />
                     <h3 className="project-title">Portefølje nettside</h3>
                     <p className="project-description">Denne nettsiden.</p>
-                    <a href="#" className="project-link">Vis Prosjektet</a>
-                </div>
+                    <a href="https://github.com/2bias123/my-portfolio" target="_blank" className="project-link" rel="noreferrer">Ta en titt</a><img src={arrow} alt="arrow" className="arrow" />
+                </a>
             </div>
         </section>
     );
