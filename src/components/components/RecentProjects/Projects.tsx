fe22db3160ada23bfe7cd0979079a85813ec3579
Tobias <tin017@uib.no>
2023-09-30 18:57:46 +0200

    Added my travel app to the recent projects row

--- src/components/components/RecentProjects/Projects.tsx
+++ src/components/components/RecentProjects/Projects.tsx
@@ -3,6 +3,7 @@ import "./ProjectsStyle.css"
 import tauri from "./tauri.jpg"
 import codeSnippet from "./image-1.png"
 import arrow from "./arrow-up.svg"
+import project0 from "./Angels_landing_project0.png"
 
 
 const Projects: React.FC = () => {
@@ -20,6 +21,17 @@ const Projects: React.FC = () => {
                     <h3 className="project-title">Tauri Graftegner</h3>
                     <p className="project-description">Et enkelt graftegnerprogram med backend i Rust og frontend i React.ts.</p>
                     <a href="https://github.com/2bias123/first-tauri" target="_blank" className="project-link" rel="noreferrer">Ta en titt</a><img src={arrow} alt="arrow" className="arrow" />
+                </a>
+                <a href="https://github.com/SofTob/Project0" target="_blank" className="project-container" rel="noreferrer">                    
+                    <img 
+                    src={project0}
+                    alt="project" 
+                    loading="lazy" 
+                    className="project-image"
+                    />
+                    <h3 className="project-title">Reiseapp</h3>
+                    <p className="project-description">En mobilapplikasjon for lagring og deling av reiseopplevelser</p>
+                    <a href="https://github.com/SofTob/Project0" target="_blank" className="project-link" rel="noreferrer">Ta en titt</a><img src={arrow} alt="arrow" className="arrow" />
                 </a>
                 <a href="https://github.com/2bias123/my-portfolio" target="_blank" className="project-container" rel="noreferrer">                    
                     <img 
